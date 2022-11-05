138242233a1e94787b29d689f2ca66bf0357d83f
PratyushPareek26 <77439476+PratyushPareek26@users.noreply.github.com>
2022-11-05 19:11:39 +0530

    Update densenet.py

--- networks/densenet.py
+++ networks/densenet.py
@@ -7,7 +7,7 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Conv2D, Dense, Input, add, Activation, AveragePooling2D, GlobalAveragePooling2D
 from keras.layers import Lambda, concatenate
 from keras.initializers import he_normal
-from keras.layers.merge import Concatenate
+from keras.layers import Concatenate #from keras.layers.merge import Concatenate
 from keras.callbacks import LearningRateScheduler, TensorBoard, ModelCheckpoint
 from keras.models import Model, load_model
 from keras import optimizers
